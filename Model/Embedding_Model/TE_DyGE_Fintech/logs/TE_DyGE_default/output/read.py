1b3fda09eaccef000ef39733e89f38ca5c7b4f09
WANG Liane <lw1619@ic.ac.uk>
2023-06-27 17:24:00 +0800

    rm unuseful files

--- Model/Embedding_Model/TE_DyGE_Fintech/logs/TE_DyGE_default/output/read.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import glob
-import pandas as pd
-import json
-
-npz_data = np.load('/export/data/liane/HKUST_KAISA/mission21_seld/TE_DyGE_fin/logs/TE_DyGE_default/output/default_embs_FinKG_37.npz')
-print(npz_data['data'][0],len(npz_data['data']))
